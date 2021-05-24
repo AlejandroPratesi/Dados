569b30afd18e519487a70e1b904eb363c63b75f3
alejandropratesi <aspratesi@gmail.com>
2021-05-24 14:07:14 -0300

    Juego de dados.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dados';
+  
+  dadoIzquierda = '../assets/imagenes/lado1.png';
+  dadoDerecha = '../assets/imagenes/lado4.png';
+
+  numero1 = 1;
+  numero2 = 2;
+
+  tirarDados():void{
+    this.numero1 = Math.round(Math.random() * 5 + 1);
+    this.numero2 = Math.round(Math.random() * 5 + 1);
+
+    this.dadoIzquierda = '../assets/imagenes/lado' + this.numero1 + '.png';
+    this.dadoDerecha = '../assets/imagenes/lado' + this.numero2 + '.png';
+
+    console.log(this.numero1);
+    console.log(this.numero2);
+  }
+
 }
